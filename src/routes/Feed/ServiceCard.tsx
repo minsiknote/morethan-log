d1cd0f5aa1e06a7488d102653252f2f9eed6cf86
이민식 <104414152+minsiknote@users.noreply.github.com>
2024-08-03 03:37:16 +0900

    Update ServiceCard.tsx

--- src/routes/Feed/ServiceCard.tsx
+++ src/routes/Feed/ServiceCard.tsx
@@ -4,30 +4,6 @@ import { AiFillCodeSandboxCircle } from "react-icons/ai"
 import styled from "@emotion/styled"
 import { Emoji } from "src/components/Emoji"
 
-const ServiceCard: React.FC = () => {
-  if (!CONFIG.projects) return null
-  return (
-    <>
-      <StyledTitle>
-        <Emoji>🌟</Emoji> Service
-      </StyledTitle>
-      <StyledWrapper>
-        {CONFIG.projects.map((project, idx) => (
-          <a
-            key={idx}
-            href={`${project.href}`}
-            rel="noreferrer"
-            target="_blank"
-          >
-            <AiFillCodeSandboxCircle className="icon" />
-            <div className="name">{project.name}</div>
-          </a>
-        ))}
-      </StyledWrapper>
-    </>
-  )
-}
-
 export default ServiceCard
 
 const StyledTitle = styled.div`
